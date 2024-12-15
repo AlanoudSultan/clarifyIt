60a98aa2c2f2d57caf0d30fefa90a8859bc3e54b
Alanoud <abaalkhailalanoud@gmail.com>
2024-12-15 17:18:36 +0300

    Initial Commit

---
+++ clarifyIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clarifyIt
+//
+//  Created by Alanoud Abaalkhail on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clarifyIt/clarifyItApp.swift
@@ -0,0 +1,17 @@
+//
+//  clarifyItApp.swift
+//  clarifyIt
+//
+//  Created by Alanoud Abaalkhail on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct clarifyItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
