edee0a7f8e02acf7bd8207061043204598e178b0
Nujud <Nujudalsaad@gmail.com>
2024-12-17 17:01:46 +0300

    Link ContentView with Splash pages

--- clarifyIt/Splash.swift
+++ clarifyIt/Splash.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  Splash.swift
 //  clarifyIt
@@ -8,11 +10,41 @@
 import SwiftUI
 
 struct Splash: View {
+    @State private var isActive = false
+    @State private var scale: CGFloat = 1.0
+    private let duration: Double = 2.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+            ZStack{
+                LinearGradient(gradient: .init(colors: [Color.splashT, Color.splashB]), startPoint: .center, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    
+                    Text("ClarifyIt")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.purpleSplash)
+                        .padding(-20)
+                    
+                    Image("ClarifyIt")
+                        .resizable()
+                        .frame(width: 350, height: 350)
+                        .scaleEffect(scale)
+                        .onAppear{
+                            withAnimation(.easeIn(duration: 1.5)) {
+                                scale = 1.2} }
+                    
+                    Text("Feel free to ask and learn")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                }
+        }
     }
 }
-
 #Preview {
     Splash()
 }
