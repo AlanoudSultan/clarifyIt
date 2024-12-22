1d56cd46fbf7c546e8e70978daf98d63b18116dd
Nujud <Nujudalsaad@gmail.com>
2024-12-22 16:06:08 +0300

    Linking contentView with Splash & MainView with sizes adjusting + some of Streak

--- clarifyIt/Streak.swift
+++ clarifyIt/Streak.swift
@@ -1,18 +1,18 @@
 //
-//  Beginning.swift
+//  Streak.swift
 //  clarifyIt
 //
-//  Created by Nujud Abdullah on 18/06/1446 AH.
+//  Created by Nujud Abdullah on 19/06/1446 AH.
 //
 
 import SwiftUI
 
-struct Beginning: View {
+struct Streak: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Beginning()
+    Streak()
 }
