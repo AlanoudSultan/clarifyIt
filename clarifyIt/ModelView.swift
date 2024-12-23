7ec436fd93c6636528e939b9b39a7d86c5e9f6dc
Alanoud <abaalkhailalanoud@gmail.com>
2024-12-23 14:46:38 +0300

    Model view file added

---
+++ clarifyIt/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  clarifyIt
+//
+//  Created by Alanoud Abaalkhail on 22/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
