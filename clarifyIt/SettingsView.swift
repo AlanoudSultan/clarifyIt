7ef6cb0f1a9f9df068e4f50f300c3c727f62202d
ghadah <Ghadahx2015@gmail.com>
2024-12-25 18:49:03 +0300

    Ghadah's last version

--- clarifyIt/SettingsView.swift
+++ clarifyIt/SettingsView.swift
@@ -6,13 +6,113 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct SettingsView: View {
+    
+  //  @Query(sort:\ DataModel.name) var dataModel: [DataModel]
+    //bindable
+    @Bindable var user: DataModel
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    
+//    @State public var selectedLanguage: String = ""
+//    @State public var updatedName: String = ""
+//    @State public var understandingLevel: String = ""
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+                Form {
+                    VStack (alignment: .leading){
+                        Spacer()
+                        Spacer()
+                        
+                        
+                        HStack(spacing: 40){
+                            Text("Name: ")
+                                .font(.custom("SF Pro Text", size: 16)).fontWeight(.medium)
+                            //MARK: remove double couts
+                         //   TextField(dataModel.last!.name , text: $updatedName)
+                            TextField("", text: $user.name)
+                            
+                            
+                        }
+                        Divider()
+                        Spacer()
+                        Spacer()
+                        
+                        
+                        Text("Language of Learning")
+                            .font(.custom("SF Pro Text", size: 16)).fontWeight(.medium)
+                        Spacer()
+                        
+                        Picker("Landuage of Learning", selection: $user.selectedLanguage) {
+                                    
+                                    Text("Arabic").tag("Arabic")
+                                    Text("English").tag("English")
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                        
+                        Spacer()
+                        Spacer()
+                        
+                        
+                        Text("Level of Understanding Words")
+                            .font(.custom("SF Pro Text", size: 16)).fontWeight(.medium)
+                        Spacer()
+                        Picker("Understanding Level", selection: $user.understandingLevel) {
+                            Text("Low").tag("Low")
+                            Text("Middle").tag("Middle")
+                            Text("High").tag("High")
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        
+                        Spacer()
+                        Spacer()
+                    }
+                    .scrollContentBackground(.hidden)
+                } //VStack close
+                
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Done") {
+                            // Handle save action here
+//                            dataModel.last!.name = updatedName
+                            
+                            
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                        .foregroundColor(.accentColor)
+                        .bold()
+                    }
+                }
+                .navigationBarBackButtonHidden(true)
+            } //NavigationView Close
+            
+//            .onAppear {
+//                
+//                updatedName = dataModel.name
+//                selectedLanguage = dataModel.selectedLanguage
+//                understandingLevel = dataModel.understandingLevel
+//            }
+            
+        }
     }
 }
 
-#Preview {
-    SettingsView()
-}
+
+    
+//
+//#Preview {
+//    SettingsView()
+//}
